b8630f6c61f1b786a592c656eb665c3c275daee6
Adriano Anschau <adrianoanschau@gmail.com>
2019-09-02 13:28:59 -0300

    fix: Corrige erro de import para serviço de cache

--- src/app/services/cache.interceptor.spec.ts
+++ src/app/services/cache.interceptor.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CacheInterceptorService } from './cache.interceptor';
+import { CacheInterceptor } from './cache.interceptor';
 
 describe('CacheInterceptorService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: CacheInterceptorService = TestBed.get(CacheInterceptorService);
+    const service: CacheInterceptor = TestBed.get(CacheInterceptor);
     expect(service).toBeTruthy();
   });
 });
